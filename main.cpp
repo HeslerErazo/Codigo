076d0501739f2424f7ffb794cab0f101a11cabe3
HeslerErazo <erazohesler@gmail.com>
2021-02-21 13:02:26 -0600

    Proyecto Unidad I

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "startShip.h"
+#include "snake.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    system("cls");
+    
+    int opcion=0;
+   
+    while(true){
+    cout <<"-----------------------------------------------" << endl;
+    cout <<"-                Menú de Juegos              -" << endl;
+    cout <<"-----------------------------------------------" << endl;
+    cout << endl;
+    cout << "Ingrese una opcion: " << endl;
+    cout << "1 - Startship" << endl;
+    cout << "2 - Snake" << endl;
+
+    cin >> opcion;
+   
+    switch(opcion){
+
+    case 1: starShip();
+    break;
+    case 2: snake();
+    break;
+    default:   
+    break;
+    }
+} if(opcion==0){
+    system("pause");
+}
+    return 0;
+}
+
+
